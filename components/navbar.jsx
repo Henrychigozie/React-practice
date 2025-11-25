7aae33800c08da23aa9956b05da4823cb577039c
Henry <h85033898@gmail.com>
2025-11-25 21:21:58 +0100

    first commit

---
+++ components/navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar() {
+  return (
+    <>
+      <nav className="henry">
+        <p>Home</p>
+        <p>Page</p>
+        <p>Footer</p>
+      </nav>
+    </>
+  );
+}
+
+import App from "../src/App";
+export default Navbar;
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/navbar";
+function App() {
+  return (
+    <>
+      <Navbar />
+      <p>Ben</p>
+    </>
+  );
+}
+
+export default App;
